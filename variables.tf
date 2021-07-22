ad3ce17333b87c7609ed41fbbb8f216c483004b6
PRAKASH3794 <41152550+PRAKASH3794@users.noreply.github.com>
2021-07-22 21:29:43 +0530

    update variables

--- variables.tf
+++ variables.tf
@@ -1,9 +1,12 @@
 variable "access_key" {
+  default = "AKIA3HC3WOYYBJBINPYT"
   description = "access_key"
   }
 variable "secret_key" {
+  default = "9mcscja2htezeBJJpVGqdNle3RFlEB8g9ECrgXxG"
   description = "secret_key"
   }
 variable "region" {
+  default = "us-east-1"
   description = "region"
   }
